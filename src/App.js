a456b025c157a64a031caa58a9520cd1a2facacc
[chetan] <[suresh.pyla2012]>
2021-02-16 09:07:09 +0530

    react task

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
-import Login from './Pages/Login'
-import NotFound from './Pages/NotFound'
-import Register from './Pages/Register'
-import Dashboard from './Pages/Dashboard'
+import Login from './components/Login'
+import NotFound from './components/NotFound'
+import Register from './components/Register'
+import Dashboard from './components/Dashboard'
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
 
 function App() {
   return (
     <Router>
       <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand href="#home">React-JWT Login</Navbar.Brand>
+        <Navbar.Brand href="#home">Login</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,12 +2,11 @@ import React, { Component } from 'react'
 import { Form, Button, Container } from 'react-bootstrap'
 const axios = require('axios')
 
-export default class Register extends Component {
+export default class Login extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
-          first_name: '',
-          last_name: '',
           email: '',
           password: '',
         };
@@ -19,15 +18,11 @@ export default class Register extends Component {
     
 
     handleSubmit(event) {
-        console.log(this.state.first_name)
-        console.log(this.state.last_name)
         console.log(this.state.email)
         console.log(this.state.password)
-        axios.post('http://localhost:8000/account/api/register',{
+        axios.post('',{
             username: this.state.email,
             password: this.state.password,
-            first_name: this.state.first_name,
-            last_name: this.state.last_name
         }).then(function (res){
             console.log(res)
             localStorage.setItem('token', res.data.access);
@@ -37,29 +32,30 @@ export default class Register extends Component {
         })
         event.preventDefault();
     }
+
     render() {
         return (
             <Container style={{ marginTop: '100px' }}>
                 <Form>
                     <Form.Group controlId="formBasicEmail" style={{ width: '300px' }}>
-                        <Form.Label>First name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter First Name" name="first_name" value={this.state.first_name} onChange={this.onChange}/>
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="text" placeholder="Enter email" name="email" value={this.state.email} onChange={this.onChange}/>
+                        <Form.Text className="text-muted">
+                        
+                        </Form.Text>
                     </Form.Group>
-                    <Form.Group controlId="formBasicEmail" style={{ width: '300px' }}>
-                        <Form.Label>Last Name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter Last Name" name="last_name" value={this.state.last_name} onChange={this.onChange}/>
-                    </Form.Group>
-                    <Form.Group controlId="formBasicEmail" style={{ width: '300px' }}>
-                        <Form.Label>Username</Form.Label>
-                        <Form.Control type="text" placeholder="Enter username" name="email" value={this.state.email} onChange={this.onChange}/>
-                    </Form.Group>
+
                     <Form.Group controlId="formBasicPassword" style={{ width: '300px' }}>
                         <Form.Label>Password</Form.Label>
                         <Form.Control type="password" placeholder="Password" name="password" value={this.state.password} onChange={this.onChange}/>
                     </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
                     <Button variant="primary" type="submit" onClick={this.handleSubmit}>
-                        Register
+                        Login
                     </Button>
+                   
                 </Form>
             </Container>
         )
